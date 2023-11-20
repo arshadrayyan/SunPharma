31c29bb48803e403508575ca8a4092cb4615732d
Application <arshadrayyan1997@gmail.com>
2023-11-20 18:29:19 +0530

    sun poly neuro clinic

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,4 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-<<<<<<< HEAD
     id("com.android.application") version "8.1.3" apply false
-=======
-    id("com.android.application") version "8.1.4" apply false
->>>>>>> 0bfa91b (sun poly neuro clinic)
 }
